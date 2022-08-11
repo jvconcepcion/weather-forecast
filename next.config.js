700a73d6a7fa62b128ee33cb6c94c83baffe9fec
Jonathan Concepcion <jonathanconcepcion1991@gmail.com>
2022-08-11 12:04:55 +0800

    fix container style and next config file

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = {
-  nextConfig,
   env: {
     BASE_URL: "https://api.openweathermap.org/data/2.5",
     API_KEY: "5354b1044c74bfa5f0866f3a3f0a06b7"
   },
 }
+
+module.exports = nextConfig
